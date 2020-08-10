a99db2c982ef66be63c889e45ac2c79668a500b2
aylacansu <cansusa81@gmail.com>
2020-08-10 15:38:09 -0500

    Test Runner

--- src/test/java/runnerTest/TestRunner.java
+++ src/test/java/runnerTest/TestRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         //Path to your feature file
         features = {"src/test/resources/features"},
         // Specify tags to be executed
-        tags = {"@login1"},
+        tags = {"@login1,@messenger"},
         //Specify step definition package name (Note: make sure to have this package on current directory)
         glue = {"stepdefinition"}
 
